56f13a3525de2af0e590b5aa9988d041f022e704
rishabh <rishabhc329@gmail.com>
2020-08-14 19:44:28 +0530

    Updates

---
+++ precache-manifest.ad684efc9c379c86644a55caacf9674a.js
@@ -0,0 +1,82 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "75b307e6bcb79576d839a790833ae4aa",
+    "url": "/Chinese-character/index.html"
+  },
+  {
+    "revision": "89ea2f9d9b3454f100b5",
+    "url": "/Chinese-character/static/css/main.3504e839.chunk.css"
+  },
+  {
+    "revision": "ddd3cdcb9d199e28ee83",
+    "url": "/Chinese-character/static/js/2.3f603bd9.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/Chinese-character/static/js/2.3f603bd9.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "89ea2f9d9b3454f100b5",
+    "url": "/Chinese-character/static/js/main.bf6ed52d.chunk.js"
+  },
+  {
+    "revision": "54014234c4195737c1d4",
+    "url": "/Chinese-character/static/js/runtime-main.7900e69f.js"
+  },
+  {
+    "revision": "260ff6d671036600e661196be1ffef2f",
+    "url": "/Chinese-character/static/media/0.260ff6d6.jpg"
+  },
+  {
+    "revision": "74642c7e52b4b87d5edbfbf86b89e1a6",
+    "url": "/Chinese-character/static/media/1.74642c7e.jpg"
+  },
+  {
+    "revision": "dbe089ec84cc636ff9f81f66cfe3dfa2",
+    "url": "/Chinese-character/static/media/2.dbe089ec.jpg"
+  },
+  {
+    "revision": "448e5cf9eecb3405c1a624f51ad64c13",
+    "url": "/Chinese-character/static/media/3.448e5cf9.jpg"
+  },
+  {
+    "revision": "54894b0447e301c49600bb9ce7e46d0e",
+    "url": "/Chinese-character/static/media/4.54894b04.jpg"
+  },
+  {
+    "revision": "50b82578a65ae35df659a6bd4ef0c8ce",
+    "url": "/Chinese-character/static/media/5.50b82578.jpg"
+  },
+  {
+    "revision": "518c2711dc3076b718f9e0021d7d305f",
+    "url": "/Chinese-character/static/media/6.518c2711.jpg"
+  },
+  {
+    "revision": "f9a360b3d44c9e7e1485be98fe87aa27",
+    "url": "/Chinese-character/static/media/7.f9a360b3.jpg"
+  },
+  {
+    "revision": "54894b0447e301c49600bb9ce7e46d0e",
+    "url": "/Chinese-character/static/media/8.54894b04.jpg"
+  },
+  {
+    "revision": "448e5cf9eecb3405c1a624f51ad64c13",
+    "url": "/Chinese-character/static/media/9.448e5cf9.jpg"
+  },
+  {
+    "revision": "f42f93f1b509afbd2ab1d6ebfa2db323",
+    "url": "/Chinese-character/static/media/a8282cd3-c7d0-48cc-8ceb-b766afb6754c.f42f93f1.jpg"
+  },
+  {
+    "revision": "04408f0d94665f2602ed116580fc3118",
+    "url": "/Chinese-character/static/media/dd972fa8-c32e-4a42-b423-f46b4d1d69a9.04408f0d.jpg"
+  },
+  {
+    "revision": "e84e598fe9b36c2df78f3e21d7539919",
+    "url": "/Chinese-character/static/media/go.e84e598f.svg"
+  },
+  {
+    "revision": "95377ac41de0762d2863eaa706da61d9",
+    "url": "/Chinese-character/static/media/icon.95377ac4.svg"
+  }
+]);
